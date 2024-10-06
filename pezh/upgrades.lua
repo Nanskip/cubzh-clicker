8c171a2bbb1dbb2c8b825d208c3d765a3376b0eb
Nanskip <nanskip.oof@gmail.com>
2024-10-06 10:34:58 +0300

    reworked upgrades.lua structure

--- pezh/upgrades.lua
+++ pezh/upgrades.lua
@@ -1,16 +1,22 @@
 
-local upgrades = {
-    {
-        name = "Test 1",
-        description = "Test desctiption 1",
-        cost = 100,
-        image = "pezh_upgrade_1",
-        type = "coins_per_click",
-        action = "+"
-        value = 1,
-        cost_action = "+",
-        cost_value = 100,
-    },
-}
+local upgrades = {}
+
+upgrades.get = function()
+    local data = {
+        {
+            name = "Test 1",
+            description = "Test desctiption 1",
+            cost = 100,
+            image = "pezh_upgrade_1",
+            type = "coins_per_click",
+            action = "+"
+            value = 1,
+            cost_action = "+",
+            cost_value = 100,
+        },
+    }
+
+    return data
+end
 
 return upgrades
