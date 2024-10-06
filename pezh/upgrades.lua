2d1d900c16a4661bb01b2e9f72329e8e2ddf5abe
Nanskip <nanskip.oof@gmail.com>
2024-10-06 10:50:37 +0300

    updated upgrades.lua

--- pezh/upgrades.lua
+++ pezh/upgrades.lua
@@ -1,15 +1,15 @@
 
 local upgrades = {
     {
-        name = "Test 1",
-        description = "Test desctiption 1",
+        name = "Coins per click",
+        description = "Adds +1 coins per click",
         cost = 100,
         image = "pezh_upgrade_1",
         type = "coins_per_click",
         action = "+",
         value = 1,
-        cost_action = "+",
-        cost_value = 100,
+        cost_action = "*",
+        cost_value = 1.5,
     },
 }
 
